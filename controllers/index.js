0f9d197867dd27e24ee0d4d6a321a330fe81624b
taylorkeltgen <taylorkeltgen@gmail.com>
2021-12-27 21:01:06 -0600

    completed user-route and started listing-route

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,12 @@
 // initializing router
-const router = require('express').router();
+// const router = require('express').router();
 
 // import routes
 const apiRoutes = require('./api');
 const homepageRoutes = require('./homepage.js');
 const dashboardRoutes = require('./dashboard.js');
 
-// 
+//
 router.use('/', homepageRoutes);
 router.use('dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
--- server.js
+++ server.js
@@ -22,20 +22,20 @@ const sess = {
 
 app.use(session(sess));
 
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
 //initializing handlebars
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 // initializing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // a method inbuilt in express to recognize the incoming Request Object as strings or arrays Source: https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+// app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
